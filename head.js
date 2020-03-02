45005839fb0d841c7cf4b5a932bdaaf4146a1e6c
Jaybur1 <jayburbyga@gmail.com>
2020-03-02 19:23:52 +0000

    head.js added

---
+++ head.js
@@ -0,0 +1,11 @@
+const assertEqual = (actual, expected) => {
+  actual === expected ? console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const head = arr => arr[0];
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello","Lighthouse","Labs"]), "Hello");
+assertEqual(head([1]),1);
+assertEqual(head([]), undefined);
