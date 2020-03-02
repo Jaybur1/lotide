f293f1b8610a5b6bf2b35300bbfa5d8004842ab0
Jaybur1 <jayburbyga@gmail.com>
2020-03-02 19:08:45 +0000

    assertEqual.js added

---
+++ assertEqual.js
@@ -0,0 +1,9 @@
+const assertEqual = (actual, expected) => {
+  actual === expected ? console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("hi","hi");
+assertEqual(13,15);
+
