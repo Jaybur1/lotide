f3f57c1b28b27441b00fe4cf919474e21fb40130
Jaybur1 <jayburbyga@gmail.com>
2020-03-03 21:52:26 +0000

    eqArray.js added

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = (actual, expected) => {
+  actual === expected ? console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+
+//function that takes to arrays and returns true or false, based on a perfect match
+
+const eqArrays = (arr1, arr2) => {
+  let isEqual = true;
+  arr1.forEach((element, index) => {
+    if (element !== arr2[index]) isEqual = false;
+  });
+  return isEqual;
+};
+
+
+
+//TESTS
+
+assertEqual(eqArrays(["1","2","3"],["1","2","3"]),true);
+assertEqual(eqArrays(["1","2","3"],["1","2",3]),false);
